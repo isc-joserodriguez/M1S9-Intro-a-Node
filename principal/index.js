30258557cbc227faa4356481b45c3efa54765d66
José Rodriguez <isc.joserodriguez@gmail.com>
2021-09-18 12:21:29 -0600

    Import/Export

---
+++ principal/index.js
@@ -0,0 +1,18 @@
+/* const funcionDeSuma = require('../funcionesMatematicas.js').miFuncion;
+const funcionDeMultiplicacion = require('../funcionesMatematicas.js').multiplicacion;
+const funcionDeDivision = require('../funcionesMatematicas.js').division;
+const mensaje = require('../funcionesMatematicas.js').mensaje; */
+
+/* const funcionesMatematicas = require('../funcionesMatematicas.js'); */
+
+const {
+    miFuncion,
+    multiplicacion,
+    division,
+    mensaje
+} = require('../funcionesMatematicas');
+
+
+console.log(miFuncion(1, 2));
+console.log(multiplicacion(1, 2));
+console.log(mensaje);
