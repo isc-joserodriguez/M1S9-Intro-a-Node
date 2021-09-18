a9a245f31ceaf35575f143f34eecf87079deff8d
José Rodriguez <isc.joserodriguez@gmail.com>
2021-09-18 10:08:57 -0600

    [Intro] Valores por default

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-const suma = (num1, num2) => {
+const suma = (num1=10, num2=5) => {
     return num1 + num2
 };
 
 let suma4_7 = suma(4, 7);
 let suma3_9 = suma(3, 9);
 
+let sumaX = suma(12);
+let sumaY = suma(undefined, 10);
+
 console.log(suma4_7, suma3_9);
+
+console.log(sumaX, sumaY);
